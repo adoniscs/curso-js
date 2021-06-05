da02c21a7b1e97a34b4440429553a81e1f0ada8e
Adonis Cipriano <desenvolvimento777@gmail.com>
2021-06-05 00:34:31 -0300

    aula sobre variaveis

---
+++ 01-variaveis/script-aula.js
@@ -0,0 +1,29 @@
+var nome = "Adonis";
+var idade = 25;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = "Silveira",
+  cidade = "SP";
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaoFuidefinido);
+// hoisting
+var comida;
+console.log(comida);
+comida = "Pizza";
+console.log(comida);
+
+let time = "Barcelona";
+time = "Real Madrid";
+console.log(time);
--- 01-variaveis/script.js
+++ 01-variaveis/script.js
@@ -1,1 +1,18 @@
-console.log("oi");
+// declarar uma variável com o seu nome
+
+var nome2 = "Fred";
+
+// declarar uma variável com a sua idade
+var idade2 = 1;
+
+// declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// atribuir valor a sua comida favorita
+comidaFavorita = "Ração";
+
+// declarar 5 variáveis diferentes sem valor
+var cor, altura, peso, distancia, quantidade;
+
+console.log(nome2, idade2, comidaFavorita);
